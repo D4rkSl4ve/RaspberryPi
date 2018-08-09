512a3cc3a2c97dff1a14453ef782a992b255123a
D4rkSl4ve <38662669+D4rkSl4ve@users.noreply.github.com>
2018-08-09 17:21:03 -0400

    syntax echo

--- raspi-torbox/raspi-torbox.sh
+++ raspi-torbox/raspi-torbox.sh
@@ -1677,7 +1677,7 @@ do_first_time_boot_menu() {
 }
 
 do_swap_change() {
-  echo '\n Increasing the SWAP file to 1GB for smoother performance \n date' >> /var/log/rpi-config_install.log &&
+  echo -e '\n Increasing the SWAP file to 1GB for smoother performance \n' date >> /var/log/rpi-config_install.log &&
   date >> /var/log/rpi-config_install.log &&
   echo -e "\e[1;96m>  Increasing the SWAP file to 1GB for smoother performance \n\e[0m" &&
   do_with_root sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1024/' /etc/dphys-swapfile >> /var/log/rpi-config_install.log 2>&1 &&
