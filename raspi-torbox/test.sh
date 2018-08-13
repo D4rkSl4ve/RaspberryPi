5bcdea6cb128a49752770f4115217ac250e7f7e3
D4rkSl4ve <38662669+D4rkSl4ve@users.noreply.github.com>
2018-08-13 18:23:55 -0400

    testing jackett

--- raspi-torbox/test.sh
+++ raspi-torbox/test.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 
-if (whiptail --title "Criteria to use preassigned settings" --yesno --defaultno "
-  • Rebooted after running the 'First Time Boot'
-  • Installed the 'Requirement Packages'
-  • Installed the 'TorBox Programs'
-  • Rebooted after installing the 'Required Packages and TorBox Programs'
-  • All the services have been started/opened by their respective port numbers
-    via a local browser  (ie torboxIP:port - 192.168.0.60:8989)
-    - Deluge  (torboxIP:8112)
-    - Jackett (torboxIP:9117)
-    - Sonarr  (torboxIP:8989)
-    - Radarr  (torboxIP:7878)
-    - Lidarr  (torboxIP:8686)
+      # Jackett:  program
+      echo -e '\nDownloading and installing program:  Jackett' >> /var/log/rpi-config_install.log &&
+      echo -e "\e[0;96m> Downloading and installing program:\e[0;92m  Jackett \e[0m" &&
+      cd /home/pi/Downloads
+      wget https://github.com/Jackett/Jackett/releases/download/v0.9.41/Jackett.Binaries.Mono.tar.gz >> /var/log/rpi-config_install.log 2>&1 &&
+      do_with_root tar -zxf Jackett.Binaries.Mono.tar.gz --directory /opt/ >> /var/log/rpi-config_install.log 2>&1 &&
+      do_with_root chown -Rh pi:pi /opt/Jackett &&
 
+      # Jackett:  service
+      echo -e '\nCreating service for:  Jackett' >> /var/log/rpi-config_install.log &&
+      echo -e "\e[0;96m> Creating service for:\e[0;92m  Jackett \e[0m" &&
+      cd /home/pi
+      cat > jackett.service << EOF
+      [Unit]
+      Description=Jackett Daemon
+      After=network.target
 
-                    Has the following criteria been met?
-    " 20 85) then
+      [Service]
+      User=pi
+      Restart=always
+      RestartSec=5
+      Type=simple
+      ExecStart=/usr/bin/mono --debug /opt/Jackett/JackettConsole.exe --NoRestart
+      TimeoutStopSec=20
+
+      [Install]
+      WantedBy=multi-user.target
+      EOF
+      do_with_root mv jackett.service /lib/systemd/system/jackett.service
+
+      echo -e '\nStarting service:  Jackett' >> /var/log/rpi-config_install.log &&
+      echo -e "\e[0;96m> Starting service:\e[0;92m  Jackett \e[0m" &&
+      sudo systemctl enable jackett >> /var/log/rpi-config_install.log &&
+      sudo systemctl start jackett &&
+      sudo systemctl status jackett >> /var/log/rpi-config_install.log &&
 
       # Jackett
       echo -e '\nDownloading and replacing file(s) for:  Jackett' >> /var/log/rpi-config_install.log &&
