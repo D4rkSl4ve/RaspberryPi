8d96ddc72e8b80395303da6706856c3e7ada2bb2
D4rkSl4ve <38662669+D4rkSl4ve@users.noreply.github.com>
2018-08-13 15:05:32 -0400

    commented out sonarr, radarr, lidarr, still haveing problems with the preassigned settings

--- raspi-torbox/raspi-torbox.sh
+++ raspi-torbox/raspi-torbox.sh
@@ -2212,51 +2212,50 @@ do_torbox_preassigned_settings() {
     sudo systemctl status jackett >> /var/log/rpi-config_install.log &&
 
     # Sonarr
-    echo -e '\nDownloading and replacing file(s) for:  Sonarr' >> /var/log/rpi-config_install.log &&
-    echo -e "\e[0;96m> Downloading and replacing file(s) for:\e[0;92m  Sonarr \e[0m" &&
-    sudo systemctl stop sonarr &&
-    cd /home/pi/.config/NzbDrone &&
-    rm config.xml && rm *.db* &&
-    wget https://raw.githubusercontent.com/D4rkSl4ve/RaspberryPi/master/raspi-torbox/sonarr/config.xml -O /home/pi/.config/NzbDrone/config.xml >> /var/log/rpi-config_install.log 2>&1 &&
-    sudo chmod 644 /home/pi/.config/NzbDrone/config.xml &&
-    wget https://raw.githubusercontent.com/D4rkSl4ve/RaspberryPi/master/raspi-torbox/sonarr/nzbdrone.db -O /home/pi/.config/NzbDrone/nzbdrone.db >> /var/log/rpi-config_install.log 2>&1 &&
-    sudo chmod 644 /home/pi/.config/NzbDrone/nzbdrone.db &&
-    wget https://raw.githubusercontent.com/D4rkSl4ve/RaspberryPi/master/raspi-torbox/sonarr/nzbdrone.db-journal -O /home/pi/.config/NzbDrone/nzbdrone.db-journal >> /var/log/rpi-config_install.log 2>&1 &&
-    sudo chmod 644 /home/pi/.config/NzbDrone/nzbdrone.db-journal &&
-    sudo systemctl start sonarr &&
-    sudo systemctl status sonarr >> /var/log/rpi-config_install.log &&
-    echo -e "\e[0;96m> The \e[0;92mAPI Key has to be reset \e[0;96mat Settings/General, generate New API Key\e[0m" &&
+    #echo -e '\nDownloading and replacing file(s) for:  Sonarr' >> /var/log/rpi-config_install.log &&
+    #echo -e "\e[0;96m> Downloading and replacing file(s) for:\e[0;92m  Sonarr \e[0m" &&
+    #sudo systemctl stop sonarr &&
+    #cd /home/pi/.config/NzbDrone &&
+    #rm config.xml && rm *.db* &&
+    #wget https://raw.githubusercontent.com/D4rkSl4ve/RaspberryPi/master/raspi-torbox/sonarr/config.xml -O /home/pi/.config/NzbDrone/config.xml >> /var/log/rpi-config_install.log 2>&1 &&
+    #sudo chmod 644 /home/pi/.config/NzbDrone/config.xml &&
+    #wget https://raw.githubusercontent.com/D4rkSl4ve/RaspberryPi/master/raspi-torbox/sonarr/nzbdrone.db -O /home/pi/.config/NzbDrone/nzbdrone.db >> /var/log/rpi-config_install.log 2>&1 &&
+    #sudo chmod 644 /home/pi/.config/NzbDrone/nzbdrone.db &&
+    #wget https://raw.githubusercontent.com/D4rkSl4ve/RaspberryPi/master/raspi-torbox/sonarr/nzbdrone.db-journal -O /home/pi/.config/NzbDrone/nzbdrone.db-journal >> /var/log/rpi-config_install.log 2>&1 &&
+    #sudo chmod 644 /home/pi/.config/NzbDrone/nzbdrone.db-journal &&
+    #sudo systemctl start sonarr &&
+    #sudo systemctl status sonarr >> /var/log/rpi-config_install.log &&
+    #echo -e "\e[0;96m> The \e[0;92mAPI Key has to be reset \e[0;96mat Settings/General, generate New API Key\e[0m" &&
 
     # Radarr
-    echo -e '\nDownloading and replacing file(s) for:  Radarr' >> /var/log/rpi-config_install.log &&
-    echo -e "\e[0;96m> Downloading and replacing file(s) for:\e[0;92m  Radarr \e[0m" &&
-    sudo systemctl stop radarr &&
-    cd /home/pi/.config/Radarr &&
-    rm config.xml && rm *.db* &&
-    wget https://raw.githubusercontent.com/D4rkSl4ve/RaspberryPi/master/raspi-torbox/radarr/config.xml -O /home/pi/.config/Radarr/config.xml >> /var/log/rpi-config_install.log 2>&1 &&
-    sudo chmod 644 /home/pi/.config/Radarr/config.xml &&
-    wget https://raw.githubusercontent.com/D4rkSl4ve/RaspberryPi/master/raspi-torbox/radarr/nzbdrone.db -O /home/pi/.config/Radarr/nzbdrone.db >> /var/log/rpi-config_install.log 2>&1 &&
-    sudo chmod 644 /home/pi/.config/Radarr/nzbdrone.db &&
-    wget https://raw.githubusercontent.com/D4rkSl4ve/RaspberryPi/master/raspi-torbox/radarr/nzbdrone.db-journal -O /home/pi/.config/Radarr/nzbdrone.db-journal >> /var/log/rpi-config_install.log 2>&1 &&
-    sudo chmod 644 /home/pi/.config/Radarr/nzbdrone.db-journal &&
-    sudo systemctl start radarr &&
-    sudo systemctl status radarr >> /var/log/rpi-config_install.log &&
-    echo -e "\e[0;96m> The \e[0;92mAPI Key has to be reset \e[0;96mat Settings/General, generate New API Key\e[0m" &&
+    #echo -e '\nDownloading and replacing file(s) for:  Radarr' >> /var/log/rpi-config_install.log &&
+    #echo -e "\e[0;96m> Downloading and replacing file(s) for:\e[0;92m  Radarr \e[0m" &&
+    #sudo systemctl stop radarr &&
+    #cd /home/pi/.config/Radarr &&
+    #rm config.xml && rm *.db* &&
+    #wget https://raw.githubusercontent.com/D4rkSl4ve/RaspberryPi/master/raspi-torbox/radarr/config.xml -O /home/pi/.config/Radarr/config.xml >> /var/log/rpi-config_install.log 2>&1 &&
+    #sudo chmod 644 /home/pi/.config/Radarr/config.xml &&
+    #wget https://raw.githubusercontent.com/D4rkSl4ve/RaspberryPi/master/raspi-torbox/radarr/nzbdrone.db -O /home/pi/.config/Radarr/nzbdrone.db >> /var/log/rpi-config_install.log 2>&1 &&
+    #sudo chmod 644 /home/pi/.config/Radarr/nzbdrone.db &&
+    #wget https://raw.githubusercontent.com/D4rkSl4ve/RaspberryPi/master/raspi-torbox/radarr/nzbdrone.db-journal -O /home/pi/.config/Radarr/nzbdrone.db-journal >> /var/log/rpi-config_install.log 2>&1 &&
+    #sudo chmod 644 /home/pi/.config/Radarr/nzbdrone.db-journal &&
+    #sudo systemctl start radarr &&
+    #sudo systemctl status radarr >> /var/log/rpi-config_install.log &&
+    #echo -e "\e[0;96m> The \e[0;92mAPI Key has to be reset \e[0;96mat Settings/General, generate New API Key\e[0m" &&
 
     # lidarr
-    echo -e '\nDownloading and replacing file(s) for:  Lidarr' >> /var/log/rpi-config_install.log &&
-    echo -e "\e[0;96m> Downloading and replacing file(s) for:\e[0;92m  Lidarr \e[0m" &&
-    # removed the && from both stop/start for testing
-    sudo systemctl stop lidarr &&
-    cd /home/pi/.config/Lidarr &&
-    rm config.xml && rm *.db* &&
-    wget https://raw.githubusercontent.com/D4rkSl4ve/RaspberryPi/master/raspi-torbox/lidarr/config.xml -O /home/pi/.config/Lidarr/config.xml >> /var/log/rpi-config_install.log 2>&1 &&
-    sudo chmod 644 /home/pi/.config/Lidarr/config.xml &&
-    wget https://raw.githubusercontent.com/D4rkSl4ve/RaspberryPi/master/raspi-torbox/lidarr/lidarr.db -O /home/pi/.config/Lidarr/lidarr.db >> /var/log/rpi-config_install.log 2>&1 &&
-    sudo chmod 644 /home/pi/.config/Lidarr/lidarr.db &&
-    sudo systemctl start lidarr &&
-    sudo systemctl status lidarr >> /var/log/rpi-config_install.log &&
-    echo -e "\e[0;96m> The \e[0;92mAPI Key has to be reset \e[0;96mat Settings/General, generate New API Key\e[0m"
+    #echo -e '\nDownloading and replacing file(s) for:  Lidarr' >> /var/log/rpi-config_install.log &&
+    #echo -e "\e[0;96m> Downloading and replacing file(s) for:\e[0;92m  Lidarr \e[0m" &&
+    #sudo systemctl stop lidarr &&
+    #cd /home/pi/.config/Lidarr &&
+    #rm config.xml && rm *.db* &&
+    #wget https://raw.githubusercontent.com/D4rkSl4ve/RaspberryPi/master/raspi-torbox/lidarr/config.xml -O /home/pi/.config/Lidarr/config.xml >> /var/log/rpi-config_install.log 2>&1 &&
+    #sudo chmod 644 /home/pi/.config/Lidarr/config.xml &&
+    #wget https://raw.githubusercontent.com/D4rkSl4ve/RaspberryPi/master/raspi-torbox/lidarr/lidarr.db -O /home/pi/.config/Lidarr/lidarr.db >> /var/log/rpi-config_install.log 2>&1 &&
+    #sudo chmod 644 /home/pi/.config/Lidarr/lidarr.db &&
+    #sudo systemctl start lidarr &&
+    #sudo systemctl status lidarr >> /var/log/rpi-config_install.log &&
+    #echo -e "\e[0;96m> The \e[0;92mAPI Key has to be reset \e[0;96mat Settings/General, generate New API Key\e[0m"
 
     ASK_TO_REBOOT=1
 
