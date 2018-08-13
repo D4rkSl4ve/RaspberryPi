27bfac02cde6a3bea19784b1ea0cc5122ee9ea7e
D4rkSl4ve <38662669+D4rkSl4ve@users.noreply.github.com>
2018-08-13 13:57:34 -0400

    added to Jackett Update Prerelease to true

--- raspi-torbox/raspi-torbox.sh
+++ raspi-torbox/raspi-torbox.sh
@@ -2199,6 +2199,7 @@ do_torbox_preassigned_settings() {
     echo -e "\e[0;96m> Downloading and replacing file(s) for:\e[0;92m  Jackett \e[0m" &&
     sudo systemctl stop jackett &&
     sed -i 's+"BasePathOverride": null,+"BasePathOverride": "/jackett",+' /home/pi/.config/Jackett/ServerConfig.json >> /var/log/rpi-config_install.log 2>&1 &&
+    sed -i 's+"UpdatePrerelease": null,+"UpdatePrerelease": true,+' /home/pi/.config/Jackett/ServerConfig.json >> /var/log/rpi-config_install.log 2>&1 &&
     sudo systemctl start jackett &&
     sudo systemctl status jackett >> /var/log/rpi-config_install.log &&
 
